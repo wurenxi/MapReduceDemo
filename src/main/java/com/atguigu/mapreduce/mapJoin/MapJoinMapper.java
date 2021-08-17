3a0c37fc8dd46d456eb5e08de9e1852ed20ade11
Niye <542014476@qq.com>
2021-08-18 06:33:02 +0800

    mapJoin commit

---
+++ src/main/java/com/atguigu/mapreduce/mapJoin/MapJoinMapper.java
@@ -0,0 +1,68 @@
+package com.atguigu.mapreduce.mapJoin;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author gxl
+ * @date 2021年08月17日 21:13
+ */
+public class MapJoinMapper extends Mapper<LongWritable, Text,Text, NullWritable> {
+
+    private Map<String, String> pdMap = new HashMap<>();
+    private Text outK = new Text();
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        //获取缓存的文件，并把文件内容封装到集合 pd.txt
+        URI[] cacheFiles = context.getCacheFiles();
+
+        FileSystem fs = FileSystem.get(context.getConfiguration());
+        FSDataInputStream fis = fs.open(new Path(cacheFiles[0]));
+
+        //从流中读取数据
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+
+        String line;
+        while (StringUtils.isNotEmpty(line=reader.readLine())){
+            //切割
+            String[] fields = line.split("\t");
+
+            pdMap.put(fields[0],fields[1]);
+        }
+
+        //关流
+        IOUtils.closeStream(reader);
+
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //处理 order.txt
+        String line = value.toString();
+        String[] split = line.split("\t");
+
+        //获取pid
+        String pname = pdMap.get(split[1]);
+
+        //获取订单id和订单数量
+        //封装
+        outK.set(split[0]+"\t"+pname+"\t"+split[2]);
+
+        context.write(outK,NullWritable.get());
+    }
+}
